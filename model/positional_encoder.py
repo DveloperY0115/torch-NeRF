326cd2ba0051bc8b04bfc47e427ec5c13788455c
Seungwoo Yoo <dreamy1534@gmail.com>
2021-08-30 13:50:03 +0900

    Implement minimal positional encoder

--- model/positional_encoder.py
+++ model/positional_encoder.py
@@ -2,38 +2,55 @@
 positional_encoder.py - Class for efficient positional encoding.
 """
 
+from math import pi
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 # TODO: Implement positional encoding
 
-
-class NeRFPositionalEncoder(nn.Module):
-    def __init__(self, L):
+class NeRFPositionalEncoder(object):
+    def __init__(self, in_dim: int, L: int):
         """
         Constructor for NeRFPositionalEncoder.
 
         Args:
-        - L (int): Level of positional encoding. Usually 4 (directional) or 10 (positional).
+        - in_dim: Dimension of input data. (e.g., 3 for coordinate in 3D, 2 for view direction)
+        - L: Level of positional encoding. Usually 4 (directional) or 10 (positional).
         """
-        super(NeRFPositionalEncoder, self).__init__()
+        super().__init__()
+
         self.L = L
+        self.in_dim = in_dim
+        self.out_dim = 2 * self.L * self.in_dim
 
-        # construct matrix for positional encoding
+        # creating embedding function
+        self.embed_fns = self.create_embedding_fn()
 
-    def forward(self, x):
+    def create_embedding_fn(self):
         """
-        Forward propagation.
 
-        Note that gradient is not recorded within this code block.
+        """
+        embed_fns = []
+
+        max_freq_level = self.L
+
+        freq_bands = torch.linspace(2.0 ** 0, 2.0 ** max_freq_level, steps=self.L)
+
+        for freq in freq_bands:
+            embed_fns.append(lambda x, freq=freq: torch.sin(freq * pi * x))
+            embed_fns.append(lambda x, freq=freq: torch.cos(freq * pi * x))
+
+        return embed_fns
+
+    def encode(self, x: torch.Tensor) -> torch.Tensor:
+        """
 
         Args:
-        - x (torch.Tensor): Tensor of shape (B, N, C) whose elements will be encoded.
+        - x: Tensor of shape (B, N, C) whose elements will be encoded.
 
         Returns:
         - Positional encoded 'x'
         """
-
-        with torch.no_grad():
-            raise NotImplementedError
+        return torch.cat([fn(x) for fn in self.embed_fns], -1)

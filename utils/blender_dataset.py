81844ade3d3509f8eb3dc2f10e973af65c2727d1
Seungwoo Yoo <dreamy1534@gmail.com>
2021-08-30 23:19:01 +0900

    Add file 'blender_dataset.py'

---
+++ utils/blender_dataset.py
@@ -0,0 +1,42 @@
+"""
+blender_dataset.py - Abstraction on Pytorch dataset.
+"""
+
+from json import load
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+
+from .load_blender import load_blender_data
+
+
+class BlenderDataset(data.Dataset):
+    def __init__(self, root_dir: str) -> None:
+        super().__init__()
+
+        (
+            self.imgs,
+            self.poses,
+            self.render_poses,
+            self.camera_params,
+            self.i_split,
+        ) = load_blender_data(root_dir)
+
+        self.img_height = self.camera_params[0]
+        self.img_width = self.camera_params[1]
+        self.focal_length = self.camera_params[2]
+
+        assert (
+            self.imgs[0] == self.poses[0] and self.poses[0] == self.render_poses[0]
+        ), "[!] Dataset sizes do not match."
+
+        print("==============================")
+        print("[!] Loaded data successfully.")
+        print("==============================")
+
+    def __len__(self):
+        return self.imgs[0]
+
+    def __getitem__(self, index):
+        return self.imgs[index], self.poses[index], self.render_poses[index]

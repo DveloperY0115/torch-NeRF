44e38d1d24d672177021eff9732c3a42e8c835fb
Seungwoo Yoo <dreamy1534@gmail.com>
2021-09-02 11:21:40 +0900

    Change 'W', 'H' in arguments

--- utils/render_utils.py
+++ utils/render_utils.py
@@ -8,19 +8,19 @@ import torch
 import torch.nn.functional as F
 
 
-def generate_rays(H: int, W: int, K: int, E: torch.Tensor) -> Tuple[torch.Tensor]:
+def generate_rays(W: int, H: int, K: int, E: torch.Tensor) -> Tuple[torch.Tensor]:
     """
     Generate rays given viewport dimensions, camera intrinsic, and camera extrinsic.
 
     Args:
-    - H: Height of viewport.
     - W: Width of viewport.
+    - H: Height of viewport.
     - K: Tensor of shape (4, 4) representing camera intrinsic (i.e., projection matrix).
     - E: Tensor of shape (4, 4) representing eye matrix (i.e, camera-to-world matrix).
 
     Returns:
-    - rays_origin: Tensor of shape (800, 800, 3) representing the origin of rays.
-    - rays_direction: Tensor of shape (800, 800, 3) representing the direction of rays.
+    - rays_origin: Tensor of shape (W, H, 3) representing the origin of rays.
+    - rays_direction: Tensor of shape (W, H, 3) representing the direction of rays.
     """
     y, x = torch.meshgrid(
         torch.linspace(0, W - 1, W), torch.linspace(0, H - 1, H)
@@ -40,17 +40,19 @@ def generate_rays(H: int, W: int, K: int, E: torch.Tensor) -> Tuple[torch.Tensor
     # translate the camera position.
     rays_origin = E[:3, -1].expand(rays_direction.shape)
 
+    assert rays_direction.shape == rays_origin, "[!] The ray origin-direction pair must match"
+
     return rays_origin, rays_direction
 
 
-def render(H: int, W: int, K: torch.Tensor, E: torch.Tensor) -> torch.Tensor:
+def render(W: int, H: int, K: torch.Tensor, E: torch.Tensor) -> torch.Tensor:
     """
     Render a scene.
 
     Args:
-    - H: Height of viewport.
     - W: Width of viewport.
+    - H: Height of viewport.
     - K: Tensor of shape (B, 4, 4) representing camera intrinsic (i.e., projection matrix).
     - E: Tensor of shape (B, 4, 4) representing eye matrix (i.e, camera-to-world matrix).
     """
-    rays_origin, rays_direction = generate_rays(H, W, K, E)
+    rays_origin, rays_direction = generate_rays(W, H, K, E)

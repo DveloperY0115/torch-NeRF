1b97d1d7bc7d89d1c4a9b7d9875d438c20f0e602
Seungwoo Yoo <dreamy1534@gmail.com>
2021-09-04 00:39:59 +0900

    Add file 'nerf_trainer.py'

---
+++ training/nerf_trainer.py
@@ -0,0 +1,98 @@
+from argparse import Namespace
+from typing import Tuple
+import wandb
+
+import torch
+import torch.optim as optim
+import torch.utils.data as data
+
+from pytorch3d.structures import Volumes
+from pytorch3d.transforms import so3_exp_map
+from pytorch3d.renderer import (
+    FoVPerspectiveCameras,
+    NDCGridRaysampler,
+    MonteCarloRaysampler,
+    EmissionAbsorptionRaymarcher,
+    ImplicitRenderer,
+    RayBundle,
+    ray_bundle_to_ray_points,
+)
+
+from model.nerf_cls import NeRFCls
+from utils.blender_dataset import BlenderDataset
+from .base_trainer import BaseTrainer
+
+
+class NeRFTrainer(BaseTrainer):
+    def __init__(self, opts: Namespace, checkpoint: str = None):
+        super().__init__(opts=opts)
+
+        # construct neural radiance field
+        self.model = NeRFCls(pos_dim=3, view_dir_dim=3, L_pos=10, L_direction=4)
+        self.model = self.model.to(self.device)
+
+        # optimizer & learning rate scheduler
+        self.optimizer = self.configure_optimizer()
+        self.lr_scheduler = self.configure_lr_scheduler()
+
+        # dataset & data loader
+        self.train_dataset, self.test_dataset = self.configure_dataset()
+        self.train_loader, self.test_loader = self.configure_dataloader()
+
+        # load checkpoint if available
+        if checkpoint is not None:
+            if self.load_checkpoint(checkpoint):
+                print("[!] Successfully loaded checkpoint at {}".format(checkpoint))
+            else:
+                print("[!] Failed to load checkpoint at {}".format(checkpoint))
+        else:
+            print("[!] Start without checkpoint")
+
+        # initialize W&B if requested
+        if self.opts.log_wandb:
+            wandb.init(project="NeRF-{}-{}".format(str(type(self.model)), self.opts.dataset_type))
+
+    def train(self):
+        pass
+
+    def train_one_epoch(self):
+        pass
+
+    def test_one_epoch(self):
+        pass
+
+    def configure_optimizer(self) -> torch.optim.Optimizer:
+        optimizer = optim.Adam(self.model.parameters(), lr=self.opts.lr)
+        return optimizer
+
+    def configure_lr_scheduler(self) -> torch.optim.lr_scheduler:
+        return None
+
+    def configure_dataset(self) -> Tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
+
+        if self.opts.dataset == "Blender":
+            dataset = BlenderDataset(self.opts.dataset_dir)
+
+        else:
+            # TODO: Support other kinds of datasets
+            pass
+
+        print("[!] Dataset used: {}".format(self.opts.dataset))
+
+        return train_dataset, test_dataset
+
+    def configure_dataloader(
+        self,
+    ) -> Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
+        train_loader = data.DataLoader(
+            self.train_dataset,
+            batch_size=self.opts.batch_size,
+            shuffle=True,
+            num_workers=self.opts.num_workers,
+        )
+
+        test_loader = data.DataLoader(
+            self.test_dataset, batch_size=self.opts.test_dataset_size, shuffle=False
+        )
+
+        return train_loader, test_loader

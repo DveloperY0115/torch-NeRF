03f0260310d819dbd369371ddd30952acd7a7ad3
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-06-16 22:00:51 +0900

    Add class 'Camera'

---
+++ src/renderer/camera.py
@@ -0,0 +1,37 @@
+"""
+Camera classes used inside renderer(s).
+"""
+
+import torch
+
+
+class Camera(object):
+    """
+    Basic camera class.
+
+    Attributes:
+        intrinsic (torch.Tensor): Tensor of shape (4, 4) representing an intrinsic matrix.
+        extrinsic (torch.Tensor): Tensor of shape (4, 4) representing an extrinsic matrix.
+
+    Args:
+        intrinsic (torch.Tensor): Tensor of shape (4, 4) representing an intrinsic matrix.
+        extrinsic (torch.Tensor): Tensor of shape (4, 4) representing an extrinsic matrix.
+    """
+
+    def __init__(
+        self,
+        intrinsic: torch.Tensor,
+        extrinsic: torch.Tensor,
+    ):
+        self._intrinsic = intrinsic
+        self._extrinsic = extrinsic
+
+    @property
+    def intrinsic(self) -> torch.Tensor:
+        """Returns the intrinsic matrix of the camera."""
+        return self._intrinsic
+
+    @property
+    def extrinsic(self) -> torch.Tensor:
+        """Returns the extrinsic matrix of the camera."""
+        return self._extrinsic

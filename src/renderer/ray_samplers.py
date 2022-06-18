150517ef793a4799a5e2f2fa885315452c51b2ff
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-06-19 01:18:14 +0900

    Change behavior of '_get_ray_directions'

--- src/renderer/ray_samplers.py
+++ src/renderer/ray_samplers.py
@@ -19,25 +19,22 @@ class RaySamplerBase(object):
         self,
         pixel_coords: torch.Tensor,
         cam_intrinsic: torch.Tensor,
-        cam_extrinsic: torch.Tensor,
         normalize: bool = False,
     ) -> torch.Tensor:
         """
-        Computes view direction vectors represented in the world frame.
+        Computes view direction vectors represented in the camera frame.
 
         Args:
             pixel_coords (torch.Tensor): Tensor of shape (N, 2).
                 A flattened array of pixel coordinates.
             cam_intrinsic (torch.Tensor): Tensor of shape (4, 4).
                 A camera intrinsic matrix.
-            cam_extrinsic (torch.Tensor): Tensor of shape (4, 4).
-                A camera extrinsic matrix.
             normalize (bool). A flag for normalizing resulting vectors.
                 If true, normalize ray direction vectors to make them unit vectors.
 
         Returns:
-            An instance of torch.Tensor of shape (N, 2) containing
-            view direction vectors represented in the world frame.
+            An instance of torch.Tensor of shape (N, 3) containing
+            view direction vectors represented in the camera frame.
         """
         # (u, v) -> (x, y)
         pixel_coords = pixel_coords.float()
@@ -45,10 +42,13 @@ class RaySamplerBase(object):
         pixel_coords[:, 1] = (pixel_coords[:, 1] - cam_intrinsic[1, 2]) / cam_intrinsic[1, 1]
 
         # (x, y) -> (x, y, -1)
-        ray_dir = torch.cat([pixel_coords, -torch.ones(pixel_coords.shape[0], 1)], dim=-1)
-
-        # rotate vectors in (local) camera frame to the (global) world frame
-        ray_dir = ray_dir @ cam_extrinsic[:3, :3]
+        ray_dir = torch.cat(
+            [
+                pixel_coords,
+                -torch.ones(pixel_coords.shape[0], 1),
+            ],
+            dim=-1,
+        )
 
         if normalize:
             ray_dir /= torch.linalg.vector_norm(

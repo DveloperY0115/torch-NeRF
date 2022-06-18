9e01ac49e57da74a7c278dca4850fbcecd31e202
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-06-18 22:49:54 +0900

    Provide two options for initializing intrinsic

--- src/renderer/cameras.py
+++ src/renderer/cameras.py
@@ -12,17 +12,18 @@ class CameraBase(object):
     Basic camera class.
 
     Attributes:
-        intrinsic (torch.Tensor): Tensor of shape (4, 4) representing an intrinsic matrix.
+        intrinsic (torch.Tensor | Dict): Camera intrinsic. Can be one of:
+            1. Tensor of shape (4, 4) representing an intrinsic matrix.
+            2. Dictionary of camera intrinsic parameters whose keys are:
+                ['f_x', 'f_y', 'img_width', 'img_height']
         extrinsic (torch.Tensor): Tensor of shape (4, 4) representing an extrinsic matrix.
         z_near (float): A floating point number representing the nearest depth rendered.
         z_far (float): A floating point number representing the farthest depth rendered.
-        focal_lengths (tuple): Focal length(s) of the camera.
     """
 
     def __init__(
         self,
-        focal_lengths: typing.Tuple[float, float],
-        intrinsic: torch.Tensor,
+        intrinsic: typing.Union[torch.Tensor, typing.Dict[str, float]],
         extrinsic: torch.Tensor,
         z_near: float,
         z_far: float,
@@ -31,17 +32,97 @@ class CameraBase(object):
         Constructor of class 'CameraBase'.
 
         Args:
-            intrinsic (torch.Tensor): Tensor of shape (4, 4) representing an intrinsic matrix.
+            intrinsic (torch.Tensor | Dict): Camera intrinsic. Can be one of:
+                1. Tensor of shape (4, 4) representing an intrinsic matrix.
+                2. Dictionary of camera intrinsic parameters whose keys are:
+                    ['f_x', 'f_y', 'img_width', 'img_height']
             extrinsic (torch.Tensor): Tensor of shape (4, 4) representing an extrinsic matrix.
             z_near (float): A floating point number representing the nearest depth rendered.
             z_far (float): A floating point number representing the farthest depth rendered.
-            focal_lengths (tuple): Focal length(s) of the camera.
         """
-        self._intrinsic = intrinsic
+        if not isinstance(intrinsic, torch.Tensor, dict):
+            raise ValueError(
+                "Expected torch.Tensor of Python Dict as a camera intrinsic. "
+                f"Got {type(intrinsic)}."
+            )
+
         self._extrinsic = extrinsic
         self._z_near = z_near
         self._z_far = z_far
-        self._focal_lengths = focal_lengths
+
+        if isinstance(intrinsic, torch.Tensor):
+            if intrinsic.shape != torch.Size((4, 4)):
+                raise ValueError(f"Expected a tensor of shape (4, 4). Got {intrinsic.shape}.")
+            self._intrinsic = intrinsic
+
+            self._focal_x = intrinsic[0, 0]
+            self._focal_y = intrinsic[1, 1]
+            self._img_width = int(2 * intrinsic[0, 2])
+            self._img_height = int(2 * intrinsic[1, 2])
+        else:
+            # construct camera intrinsic matrix on the fly
+            focal_x = float(intrinsic["f_x"])
+            focal_y = float(intrinsic["f_y"])
+            img_width = float(intrinsic["img_width"])
+            img_height = float(intrinsic["img_height"])
+            self._intrinsic = self._construct_intrinsic_from_params(
+                focal_x,
+                focal_y,
+                img_width,
+                img_height,
+                z_near,
+                z_far,
+            )
+
+            self._focal_x = focal_x
+            self._focal_y = focal_y
+            self._img_width = img_width
+            self._img_height = img_height
+
+    def _construct_intrinsic_from_params(
+        self,
+        focal_x: float,
+        focal_y: float,
+        img_width: float,
+        img_height: float,
+        z_near: float,
+        z_far: float,
+    ) -> torch.Tensor:
+        """
+        Constructs the camera intrinsic matrix from given camera parameters.
+
+        Note that the third and fourth row of the resulting matrix is NOT
+        significant in the neural rendering pipeline. The matrix is only used
+        for computing the ray origin and directions. We derive the matrix for
+        notational uniformity.
+
+        Args:
+            focal_x (float): Focal length of the camera along the horizontal axis.
+            focal_y (float): Focal length of the camera along the vertical axis.
+            img_width (float): Width of the image.
+            img_height (float): Height of the image.
+            z_near (float): A floating point number representing the nearest depth rendered.
+            z_far (float): A floating point number representing the farthest depth rendered.
+
+        Returns:
+            An instance of torch.Tensor of shape (4, 4) representing the intrinsic
+            matrix of the camera.
+        """
+        intrinsic = torch.tensor(
+            [
+                [focal_x, 0.0, img_width / 2.0, 0.0],
+                [0.0, focal_y, img_height / 2.0, 0.0],
+                [
+                    0.0,
+                    0.0,
+                    -(z_near + z_far) / (z_far - z_near),
+                    -2 * z_near * z_far / (z_far - z_near),
+                ],
+                [0.0, 0.0, -1.0, 0.0],
+            ],
+            dtype=torch.float32,
+        )
+        return intrinsic
 
     @property
     def intrinsic(self) -> torch.Tensor:

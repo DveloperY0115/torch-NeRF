b85d99311b9e59dbb6105807294e2ef42bc4f67b
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-06-21 01:01:00 +0900

    Fix bug in computing frequency coefficients

--- torch_nerf/src/signal_encoder/positional_encoder.py
+++ torch_nerf/src/signal_encoder/positional_encoder.py
@@ -1,57 +1,75 @@
 """
-positional_encoder.py - Class for efficient positional encoding.
+Implementation of positional encoder proposed in NeRF (ECCV 2020).
 
-Implementation influenced by: https://github.com/yenchenlin/nerf-pytorch/blob/master/run_nerf_helpers.py
+Source: https://github.com/yenchenlin/nerf-pytorch/blob/master/run_nerf_helpers.py
 """
 
 from math import pi
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
 
-class NeRFPositionalEncoder(object):
-    def __init__(self, in_dim: int, L: int):
+class PositionalEncoder(object):
+    """
+    Implementation of positional encoding.
+
+    Attributes:
+        in_dim (int): Dimensionality of the data.
+        embed_level (int): Level of positional encoding.
+        out_dim (int): Dimensionality of the encoded data.
+    """
+
+    def __init__(
+        self,
+        in_dim: int,
+        embed_level: int,
+    ):
         """
-        Constructor for NeRFPositionalEncoder.
+        Constructor for PositionalEncoder.
 
         Args:
-        - in_dim: Dimension of input data. (e.g., 3 for coordinate in 3D, 2 for view direction)
-        - L: Level of positional encoding. Usually 4 (directional) or 10 (positional).
+            in_dim (int): Dimensionality of the data.
+            embed_level (int): Level of positional encoding.
         """
         super().__init__()
 
-        self.L = L
-        self.in_dim = in_dim
-        self.out_dim = 2 * self.L * self.in_dim
+        self._embed_level = embed_level
+        self._in_dim = in_dim
+        self._out_dim = 2 * self._embed_level * self._in_dim
 
         # creating embedding function
-        self.embed_fns = self.create_embedding_fn()
+        self._embed_fns = self._create_embedding_fn()
 
-    def create_embedding_fn(self):
+    def _create_embedding_fn(self):
         """
-        Create embedding function from given number of frequency bands and dimension of data being encoded.
+        Creates embedding function from given
+            (1) number of frequency bands;
+            (2) dimension of data being encoded;
 
-        The definition of positional encoding function is as follows:
-        f(p) = (sin(2^0 * pi * p), cos(2^0 * pi * p), ..., sin(2^{L-1} * pi * p), cos(2^{L-1} * pi * p))
-        and is computed on each (x, y, z) or (theta, phi) triplet (or tuple).
+        The positional encoding is defined as:
+        f(p) = [
+                sin(2^0 * pi * p), cos(2^0 * pi * p),
+                                ...,
+                sin(2^{L-1} * pi * p), cos(2^{L-1} * pi * p)
+            ],
+        and is computed for all components of the input vector.
 
         Thus, the form of resulting tensor is:
         f(pos) = [
-                sin(2^0 * pi * x), sin(2^0 * pi * y), sin(2^0 * pi * z),  
-                cos(2^0 * pi * x), cos(2^0 * pi * y), cos(2^0 * pi * z), 
+                sin(2^0 * pi * x), sin(2^0 * pi * y), sin(2^0 * pi * z),
+                cos(2^0 * pi * x), cos(2^0 * pi * y), cos(2^0 * pi * z),
                     ...
-                ]
-        where pos = (x, y, z)
+            ],
+        where pos = (x, y, z).
 
-        For details, please refer to section 5.1 of NeRF: Representing Scenes as Neural Radiance Fields for View Synthesis, Mildenhall et al. (ECCV 2020) 
+        For details, please refer to 'NeRF: Representing Scenes as
+        Neural Radiance Fields for View Synthesis, Mildenhall et al. (ECCV 2020)'.
         """
         embed_fns = []
 
-        max_freq_level = self.L
+        max_freq_level = self._embed_level
 
-        freq_bands = torch.linspace(2.0 ** 0, 2.0 ** max_freq_level, steps=self.L)
+        freq_bands = 2 ** torch.arange(0.0, max_freq_level, dtype=torch.float32)
 
         for freq in freq_bands:
             embed_fns.append(lambda x, freq=freq: torch.sin(freq * pi * x))
@@ -59,14 +77,16 @@ class NeRFPositionalEncoder(object):
 
         return embed_fns
 
-    def encode(self, x: torch.Tensor) -> torch.Tensor:
+    def encode(self, in_signal: torch.Tensor) -> torch.Tensor:
         """
-        Compute positional encoding on the given tensor.
+        Computes positional encoding of the given signal.
 
         Args:
-        - x: Tensor of shape (B, N, C) whose elements will be encoded.
+            in_signal: An instance of torch.Tensor of shape (N, C).
+                Input signal being encoded.
 
         Returns:
-        - Positional encoded 'x'
+            An instance of torch.Tensor of shape (N, self.out_dim).
+                The positional encoding of the input signal.
         """
-        return torch.cat([fn(x) for fn in self.embed_fns], -1)
+        return torch.cat([fn(in_signal) for fn in self._embed_fns], -1)

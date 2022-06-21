9c4342ec60378f7a2f0c955443d77ce885f068d5
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-06-21 12:39:09 +0900

    Modify function 'init_scene_repr'

--- torch_nerf/runners/runner_utils.py
+++ torch_nerf/runners/runner_utils.py
@@ -10,6 +10,7 @@ import torch_nerf.src.renderer.cameras as cameras
 import torch_nerf.src.renderer.integrators as integrators
 import torch_nerf.src.renderer.ray_samplers as ray_samplers
 from torch_nerf.src.renderer.volume_renderer import VolumeRenderer
+import torch_nerf.src.signal_encoder.positional_encoder as pe
 from torch_nerf.src.utils.data.blender_dataset import NeRFBlenderDataset
 
 
@@ -67,14 +68,21 @@ def init_scene_repr(cfg: DictConfig) -> qs.QueryStructBase:
         scene (QueryStruct): An instance of derived class of QueryStructBase.
             The scene representation.
     """
-    # initialize query structure & radiance fields
     if cfg.query_struct.type == "cube":
-        radiance_field = network.nerf_mlp.NeRFMLP(
-            cfg.network.coord_encode_level,
-            cfg.network.dir_encode_level,
+        radiance_field = network.NeRFMLP(
+            2 * cfg.signal_encoder.coord_encode_level * cfg.network.pos_dim,
+            2 * cfg.signal_encoder.dir_encode_level * cfg.network.view_dir_dim,
         )
-        scene = qs.simple_cube.QSCube(radiance_field)
+        coord_enc = pe.PositionalEncoder(
+            cfg.network.pos_dim,
+            cfg.signal_encoder.coord_encode_level,
+        )
+        dir_enc = pe.PositionalEncoder(
+            cfg.network.view_dir_dim,
+            cfg.signal_encoder.dir_encode_level,
+        )
+        scene = qs.QSCube(radiance_field)
+
+        return scene, {"coord_enc": coord_enc, "dir_enc": dir_enc}
     else:
         raise ValueError("Unsupported scene representation.")
-
-    return scene

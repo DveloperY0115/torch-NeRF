a1a62a6c1d184e3102416bd03de5689fbdee408a
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-06-23 07:41:58 +0900

    Add function 'visualize_train_scene'

--- torch_nerf/runners/run_train.py
+++ torch_nerf/runners/run_train.py
@@ -27,13 +27,14 @@ def train_one_epoch(
     scheduler,
 ) -> float:
     """
-    Training routine for one epoch.
+    Trains the scene for one epoch.
 
     Args:
         cfg (DictConfig): A config object holding parameters required
             to setup scene representation.
         scene (QueryStruct): Neural scene representation to be optimized.
         renderer (VolumeRenderer): Volume renderer used to render the scene.
+        dataset (torch.utils.data.Dataset): Dataset for training data.
         loader (torch.utils.data.DataLoader): Loader for training data.
         loss_func (torch.nn.Module): Objective function to be optimized.
         optimizer (torch.optim.Optimizer): Optimizer.
@@ -89,6 +90,57 @@ def train_one_epoch(
     return total_loss
 
 
+def visualize_train_scene(
+    cfg,
+    scene,
+    renderer,
+    dataset,
+    save_dir: str,
+):
+    """
+    Visualizes the scene being trained.
+
+    Args:
+        cfg (DictConfig): A config object holding parameters required
+            to setup scene representation.
+        scene (QueryStruct): Neural scene representation to be optimized.
+        renderer (VolumeRenderer): Volume renderer used to render the scene.
+        dataset (torch.utils.data.Dataset): Dataset for training data.
+        save_dir (str): Directory to store render outputs.
+    """
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir, exist_ok=True)
+
+    render_poses = dataset.render_poses
+
+    for view_idx, extrinsic in tqdm(enumerate(render_poses)):
+        # set the camera
+        renderer.camera = cameras.PerspectiveCamera(
+            {
+                "f_x": dataset.focal_length,
+                "f_y": dataset.focal_length,
+                "img_width": dataset.img_width,
+                "img_height": dataset.img_height,
+            },
+            extrinsic,
+            cfg.renderer.t_near,
+            cfg.renderer.t_far,
+        )
+
+        pixel_pred, _ = renderer.render_scene(
+            scene,
+            num_pixels=dataset.img_width * dataset.img_height,
+            num_samples=cfg.renderer.num_samples,
+            project_to_ndc=cfg.renderer.project_to_ndc,
+            device=torch.cuda.current_device(),
+        )
+
+        tvu.save_image(
+            pixel_pred,
+            os.path.join(save_dir, f"{str(view_idx).zfill(5)}.png"),
+        )
+
+
 @hydra.main(
     version_base=None,
     config_path="../configs",  # config file search path is relative to this script
@@ -112,7 +164,7 @@ def main(cfg: DictConfig) -> None:
             cfg, scene, renderer, dataset, loader, loss_func, optimizer, scheduler
         )
 
-        print(f"Loss {epoch}: {epoch_loss}")
+        print(f"Loss (Train) {epoch}: {epoch_loss}")
 
 
 if __name__ == "__main__":

5cd1148bcc5a466727495fa657116391538552ae
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-07-01 01:26:23 +0900

    Modify function 'load_llff_data'

--- torch_nerf/src/utils/data/load_llff.py
+++ torch_nerf/src/utils/data/load_llff.py
@@ -417,86 +417,104 @@ def spherify_poses(poses, bds):
 
 
 def load_llff_data(
-    basedir, factor=8, recenter=True, bd_factor=0.75, spherify=False, path_zflat=False
+    base_dir: str,
+    factor: int = 8,
+    recenter: bool = True,
+    bd_factor: float = 0.75,
+    spherify: bool = False,
+    path_zflat: bool = False,
 ):
+    """
+    Loads LLFF dataset given the base directory.
 
-    poses, bds, imgs = _load_data(
-        basedir, factor=factor
-    )  # factor=8 downsamples original imgs by 8x
-    print("Loaded", basedir, bds.min(), bds.max())
+    Args:
+        base_dir (str):
+        factor (int):
+        recenter (bool):
+        bd_factor (float):
+        spherify (bool):
+        path_zflat (bool):
 
-    # Correct rotation matrix ordering and move variable dim to axis 0
-    poses = np.concatenate([poses[:, 1:2, :], -poses[:, 0:1, :], poses[:, 2:, :]], 1)
-    poses = np.moveaxis(poses, -1, 0).astype(np.float32)
-    imgs = np.moveaxis(imgs, -1, 0).astype(np.float32)
-    images = imgs
-    bds = np.moveaxis(bds, -1, 0).astype(np.float32)
+    Returns:
+        imgs (np.ndarray): An instance of np.ndarray of shape ().
 
-    # Rescale if bd_factor is provided
-    sc = 1.0 if bd_factor is None else 1.0 / (bds.min() * bd_factor)
-    poses[:, :3, 3] *= sc
-    bds *= sc
+        extrinsics (np.ndarray): An instance of np.ndarray of shape ().
+
+        intrinsics (np.ndarray): An instance of np.ndarray of shape ().
+
+        z_bounds (np.ndarray): An instance of np.ndarray of shape ().
+
+        render_poses (np.ndarray): An instance of np.ndarray of shape ().
+
+        i_test (np.ndarray): An instance of np.ndarray of shape ().
+
+    """
+    imgs, extrinsics, intrinsics, z_bounds = _load_data(
+        base_dir, factor=factor
+    )  # factor = 8 downsamples original imgs by 8x
+    print("Loaded", base_dir, z_bounds.min(), z_bounds.max())
+
+    # Rescale the scene if bd_factor is provided
+    scale = 1.0 if bd_factor is None else 1.0 / (z_bounds.min() * bd_factor)
+    extrinsics[:, :3, 3] *= scale
+    z_bounds *= scale
 
     if recenter:
-        poses = recenter_poses(poses)
-
+        extrinsics = recenter_poses(extrinsics)
     if spherify:
-        poses, render_poses, bds = spherify_poses(poses, bds)
-
+        extrinsics, render_poses, z_bounds = spherify_poses(extrinsics, z_bounds)
     else:
-
-        c2w = poses_avg(poses)
-        print("recentered", c2w.shape)
-        print(c2w[:3, :4])
+        avg_camera_to_world = poses_avg(extrinsics)
+        print("recentered", avg_camera_to_world.shape)
+        print(avg_camera_to_world)
 
         ## Get spiral
         # Get average pose
-        up = normalize(poses[:, :3, 1].sum(0))
+        up_vec = normalize(extrinsics[:, :, 1].sum(0))
 
         # Find a reasonable "focus depth" for this dataset
-        close_depth, inf_depth = bds.min() * 0.9, bds.max() * 5.0
-        dt = 0.75
-        mean_dz = 1.0 / (((1.0 - dt) / close_depth + dt / inf_depth))
-        focal = mean_dz
+        close_depth, inf_depth = z_bounds.min() * 0.9, z_bounds.max() * 5.0
+        delta_t = 0.75
+        mean_delta_z = 1.0 / (((1.0 - delta_t) / close_depth + delta_t / inf_depth))
+        focal = mean_delta_z
 
         # Get radii for spiral path
-        shrink_factor = 0.8
-        zdelta = close_depth * 0.2
-        tt = poses[:, :3, 3]  # ptstocam(poses[:3,3,:].T, c2w).T
-        rads = np.percentile(np.abs(tt), 90, 0)
-        c2w_path = c2w
-        N_views = 120
-        N_rots = 2
+        position = extrinsics[:, :, 3]  # ptstocam(poses[:3,3,:].T, c2w).T
+        rads = np.percentile(np.abs(position), 90, 0)
+        camera_to_world_path = avg_camera_to_world
+        num_keyframes = 120
+        num_rotations = 2
         if path_zflat:
-            #             zloc = np.percentile(tt, 10, 0)[2]
+            # zloc = np.percentile(tt, 10, 0)[2]
             zloc = -close_depth * 0.1
-            c2w_path[:3, 3] = c2w_path[:3, 3] + zloc * c2w_path[:3, 2]
+            camera_to_world_path[:3, 3] = (
+                camera_to_world_path[:3, 3] + zloc * camera_to_world_path[:3, 2]
+            )
             rads[2] = 0.0
-            N_rots = 1
-            N_views /= 2
+            num_rotations = 1
+            num_keyframes /= 2
 
         # Generate poses for spiral path
         render_poses = render_path_spiral(
-            c2w_path,
-            up,
+            camera_to_world_path,
+            up_vec,
             rads,
             focal,
             z_rate=0.5,
-            rots=N_rots,
-            N=N_views,
+            rots=num_rotations,
+            num_keyframe=num_keyframes,
         )
 
     render_poses = np.array(render_poses).astype(np.float32)
 
-    c2w = poses_avg(poses)
-    print("Data:")
-    print(poses.shape, images.shape, bds.shape)
+    avg_camera_to_world = poses_avg(extrinsics)
+    print(f"Data: {imgs.shape}, {extrinsics.shape}, {intrinsics.shape}, {z_bounds.shape}")
 
-    dists = np.sum(np.square(c2w[:3, 3] - poses[:, :3, 3]), -1)
+    dists = np.sum(np.square(avg_camera_to_world[:3, 3] - extrinsics[:, :3, 3]), -1)
     i_test = np.argmin(dists)
     print("HOLDOUT view is", i_test)
 
-    images = images.astype(np.float32)
-    poses = poses.astype(np.float32)
+    imgs = imgs.astype(np.float32)
+    extrinsics = extrinsics.astype(np.float32)
 
-    return images, poses, bds, render_poses, i_test
+    return imgs, extrinsics, intrinsics, z_bounds, render_poses, i_test

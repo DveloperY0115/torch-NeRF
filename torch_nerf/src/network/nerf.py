1f4f3cdbbf8db61bdabc45c7dd873908ba233d35
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-07-09 01:47:39 +0900

    Rename 'nerf_mlp.py' to 'nerf.py'

--- torch_nerf/src/network/nerf.py
+++ torch_nerf/src/network/nerf.py
@@ -2,7 +2,7 @@
 Pytorch implementation of MLP used in NeRF (ECCV 2020).
 """
 
-from typing import Dict
+from typing import Tuple
 
 import torch
 import torch.nn as nn
@@ -66,7 +66,7 @@ class NeRF(nn.Module):
         self,
         pos: torch.Tensor,
         view_dir: torch.Tensor,
-    ) -> Dict[torch.Tensor, torch.Tensor]:
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Predicts color and density.
 
@@ -78,7 +78,7 @@ class NeRF(nn.Module):
             view_dir (torch.Tensor): Tensor of shape (N, self.dir_dim). View direction vectors.
 
         Returns:
-            A dict containing predicted radiance (RGB) and density (sigma) at sample points.
+            A tuple containing predicted radiance (RGB) and density (sigma) at sample points.
         """
         # check input tensors
         if (pos.ndim != 2) or (view_dir.ndim != 2):

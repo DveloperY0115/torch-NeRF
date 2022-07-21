25e933d6b542f545d179a3d08775bcbb6570bb64
Seungwoo Yoo <dreamy1534@kaist.ac.kr>
2022-07-21 23:23:41 +0900

    Clean-up entry points

--- torch_nerf/runners/run_render.py
+++ torch_nerf/runners/run_render.py
@@ -1,20 +1,13 @@
 """A script for scene rendering."""
 
-import os
 import sys
+
+import hydra
+from omegaconf import DictConfig
 
 sys.path.append(".")
 sys.path.append("..")
 
-import hydra
-from hydra.core.hydra_config import HydraConfig
-import numpy as np
-from omegaconf import DictConfig
-import torch
-from torch.utils.tensorboard import SummaryWriter
-import torchvision.utils as tvu
-from tqdm import tqdm
-import torch_nerf.src.renderer.cameras as cameras
 import torch_nerf.runners.runner_utils as runner_utils
 
 
@@ -25,45 +18,8 @@ import torch_nerf.runners.runner_utils as runner_utils
 )
 def main(cfg: DictConfig) -> None:
     """The entry point of rendering code."""
-    log_dir = os.path.join("render_out", cfg.data.dataset_type, cfg.data.scene_name)
-
-    # configure device
-    runner_utils._init_cuda(cfg)
-
-    # initialize data, renderer, and scene
-    dataset, _ = runner_utils._init_dataset_and_loader(cfg)
-    renderer = runner_utils._init_renderer(cfg)
-    scenes = runner_utils._init_scene_repr(cfg)
-
-    if cfg.train_params.ckpt.path is None:
-        raise ValueError("Checkpoint file must be provided for rendering.")
-    if not os.path.exists(cfg.train_params.ckpt.path):
-        raise ValueError("Checkpoint file does not exist.")
-
-    # load scene representation
-    _ = runner_utils._load_ckpt(
-        cfg.train_params.ckpt.path,
-        scenes,
-        optimizer=None,
-        scheduler=None,
-    )
-
-    # render
-    runner_utils._visualize_scene(
-        cfg,
-        scenes,
-        renderer,
-        intrinsics={
-            "f_x": dataset.focal_length,
-            "f_y": dataset.focal_length,
-            "img_width": dataset.img_width,
-            "img_height": dataset.img_height,
-        },
-        extrinsics=dataset.render_poses,
-        img_res=(dataset.img_height, dataset.img_width),
-        save_dir=log_dir,
-    )
-
+    render_session = runner_utils.init_session(cfg, mode="render")
+    render_session()
     print("Rendering done.")
 
 
